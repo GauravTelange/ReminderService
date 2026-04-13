f325e3aba6e27b9113849163fca77265a56dd25e
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-14 00:17:46 +0530

    Added Notification Ticket model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 const {sendBasicEmail} = require('./services/email-service');
+const cron = require('node-cron');
 
 const setUpAndStartServer = () => {
    
@@ -13,12 +14,16 @@ const setUpAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Reminder Service is running on port : ${PORT}`);
         
-        sendBasicEmail(
-            'support@admin.com',
-             'gauravtelange991@gmail.com',
-             'This is a testing email',
-             'Hey, How are you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //      'gauravtelange991@gmail.com',
+        //      'This is a testing email',
+        //      'Hey, How are you, I hope you like the support'
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+        console.log('running a task every minute');
+        });
     
     });
 }
